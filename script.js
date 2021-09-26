0bafffcc8083e51c3643c5dad82164a56b35ccd8
ngducvan9697 <ducvanit9697@gmail.com>
2021-09-26 13:58:00 +0700

    Reponsve poems, galerry section part 1

--- script.js
+++ script.js
@@ -2,20 +2,35 @@ console.log("Running script!");
 
 // import { tns } from "https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.3/min/tiny-slider.js";
 
-var slider = tns({
-  container: ".header-slider",
-  autoWidth: true,
-  mouseDrag: true,
-  swipeAngle: false,
-  speed: 400,
-  loop: true,
-  lazyload: true,
-  slideBy: 2.6,
-  autoplay: true,
-  center: true,
-  controls: false,
-  autoplayButtonOutput: false,
-});
+// var sliderHeader = tns({
+//   container: ".header-slider",
+//   autoWidth: true,
+//   mouseDrag: true,
+//   swipeAngle: false,
+//   speed: 400,
+//   loop: true,
+//   lazyload: true,
+//   slideBy: 2.6,
+//   autoplay: true,
+//   center: true,
+//   controls: false,
+//   autoplayButtonOutput: false,
+// });
+
+// var sliderHeader = tns({
+//   container: ".section-poems ul",
+//   autoWidth: true,
+//   mouseDrag: true,
+//   swipeAngle: false,
+//   speed: 400,
+//   loop: true,
+//   lazyload: true,
+//   slideBy: 2.6,
+//   autoplay: true,
+//   center: true,
+//   controls: false,
+//   autoplayButtonOutput: false,
+// });
 
 var searchContainer = document.querySelector(".search-container");
 var searchBtn = document.querySelector(".search-btn");
