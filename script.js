3329d2943e1127d9aae19bd069f9f3b448be7044
ngducvan9697 <ducvanit9697@gmail.com>
2021-09-26 12:23:28 +0700

    Update import tiny-slider

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Running script!");
 
-import { tns } from "./node_modules/tiny-slider/src/tiny-slider.js";
+// import { tns } from "https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.3/min/tiny-slider.js";
 
 var slider = tns({
   container: ".header-slider",
